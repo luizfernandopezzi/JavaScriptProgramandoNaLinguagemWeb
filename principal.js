06142762f7669a5eefc1ed06691a4de4389df997
LuizPezzi <luizfpezzi@gmail.com>
2021-04-09 00:16:04 -0300

    Atualizando html, css e js para final da Aula 2: 2.1) Criando loop for para pacientes 2.2) Alterando estilos direto no CSS com classList.add()

--- principal.js
+++ principal.js
@@ -5,8 +5,60 @@ console.log("JavaSript principal.js foi carregado!")
 var tituloprincipal = document.querySelector(".titulo-principal")
 var title = document.querySelector(".title")
 
+//var titulo = document.querySelector("h1");
+//console.log(titulo.classList);
+//resultado
+//["titulo","principal","azul"]
+
 tituloprincipal.textContent = "Luiz Pezzi - Nutrição Esportiva"
 title.textContent = tituloprincipal.textContent
+//
 
-console.log(tituloprincipal.textContent)
-console.log(title.textContent)
+//Buscando dados dos pacientes, calculando o IMC e fazendo validações dos dados inseridos
+var pacientes = document.querySelectorAll(".paciente"); //Retorna um array com todos os elementos que possuem a classe .paciente
+
+console.log(pacientes)
+
+for(var i = 0; i < pacientes.length; i++){
+
+//var pacienteI = pacientes[i]
+//console.log(pacienteI)
+
+var peso = pacientes[i].querySelector(".info-peso").textContent;
+var altura = pacientes[i].querySelector(".info-altura").textContent;
+
+var imc = (peso / (altura * altura));
+var tdImc = pacientes[i].querySelector(".info-imc");
+tdImc.textContent = imc.toFixed(2);
+//Validação dos dados inseridos na tabela
+//Pesos válidos entre 0 e 300 kg
+//Altura válida entre 0 e 2.5 m
+var pesoMin = 0;
+var pesoMax = 300;
+var alturaMin = 0;
+var alturaMax = 2.5;
+
+var validacaoPeso = true;
+if(peso < pesoMin || peso > pesoMax){
+    validacaoPeso = false
+    tdImc.textContent = "Peso inválido!"
+    //Alterando estilo (css) da linha caso os dados inseridos para peso e altura sejam inválidos
+    //paciente[i].style.backgroundColor = "red"
+    pacientes[i].classList.add("paciente-invalido");
+}
+
+var validacaoAltura = true;
+if(altura < alturaMin || altura > alturaMax){
+    validacaoAltura = false
+    tdImc.textContent = "Altura inválida!"
+    //Alterando estilo (css) da linha caso os dados inseridos para peso e altura sejam inválidos
+    pacientes[i].classList.add("paciente-invalido");
+}
+if(validacaoPeso == false && validacaoAltura == false){
+    tdImc.textContent = "Peso e altura inválidos!"
+}
+
+}
+
+ 
+
